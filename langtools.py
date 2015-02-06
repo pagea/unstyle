a8ea29e1d9030479daadb58b8b40d5db6bbec06b
Alden S. Page <pagea@allegheny.edu>
2015-02-06 15:29:43 -0500

    Fixed some typos, poked at langtools a bit

--- langtools.py
+++ langtools.py
@@ -1,13 +1,22 @@
 # Miscellaneous natural language programming functions.
+# TODO: it's kind of stupid to have curses be a dependency just because of one
+# function
 
-import nltk
+from curses.ascii import isdigit
 from nltk.corpus import cmudict
+import pylab
 
-d = smudict.dict()
+d = cmudict.dict()
 
 # Return the ESTIMATED number of syllables in a given word.
+# FIXME: Always returns 0.
 def syllableCount(word):
     if word not in d:
         return None
     else:
-        return max([len([y for y in x if isdigit(y[-1])]) for x in d[word.lower()]])
+        nsyl(word)
+
+def nsyl(word):
+    """return the max syllable count"""
+    return max([len([y for y in x if isdigit(y[-1])]) for x in d[word.lower()]])
+

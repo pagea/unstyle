a43e05adfde4a06d6e9c94ced652e2db5332b380
Alden S. Page <pagea@allegheny.edu>
2015-03-21 20:15:41 -0400

    Created improved SVM-based feature selection method.

--- stylproj/feat_select.py
+++ stylproj/feat_select.py
@@ -3,14 +3,19 @@
 """
 
 from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.svm import LinearSVC
+from sklearn.feature_selection import RFE
 from numpy import sort
 from stylproj.features.featregister import featregistry
 from collections import OrderedDict
 
 # TODO: Grid search with k-fold cross-validation on classifier in order to see
 # if better ranking results can be achieved.
-def rank_features(X, y, featureset):
-    """Rank features by their importance.
+def rank_features_dt(X, y, featureset):
+    """Rank features by their importance with a decision tree.
+
+    This does not seem to agree with rank_features_rbe, although it is
+    considerably faster. Using this with SVM classifiers is likely a bad idea.
 
     :param X: A training set of features.
     :param y: A target set (aka class labels for the training set)
@@ -33,3 +38,31 @@ def rank_features(X, y, featureset):
 
     # Sort the dictionary by value and return it. 
     return sorted(feat_importance.items(), key=lambda x:x[1], reverse=True)
+
+def rank_features_rfe(X, y, featureset):
+    """Rank features by their importance using recursive feature elimination.
+
+    :param X: A training set of features.
+    :param y: A target set (aka class labels for the training set)
+    :param featureset: An instance of a featureset (such as Basic9Extractor())
+    :rtype: An OrderedDict of the form {K : V}, with K being the feature name
+    and V being its importance. This dictionary will be sorted by importance.
+    """
+
+    # FIXME: Use an RBF SVC to rank features. It is likely that the "importance"
+    # rankings derived from a LinearSVC are similar as an RBF kernel SVM, but,
+    # for safety's sake, it is best to assume they are not.
+
+    classifier = LinearSVC()
+    classifier.fit(X, y)
+
+    ranker = RFE(classifier, 1, step=1)
+    ranker = ranker.fit(X, y)
+
+    # Get the names of the feature columns.
+    # FIXME: Duplicate code from rank_features. Make this its own function.
+    feat_importance = OrderedDict()
+    for index, func in enumerate(featureset.features):
+        feat_importance[func] = ranker.ranking_[index]
+
+    return sorted(feat_importance.items(), key=lambda x:x[1])

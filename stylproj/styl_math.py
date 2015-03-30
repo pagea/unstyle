7034cf28fe722564affd903af1ba5ece0fd07d34
Alden S. Page <pagea@allegheny.edu>
2015-03-30 01:52:51 -0400

    Added function for computing cosine distance

---
+++ stylproj/styl_math.py
@@ -0,0 +1,16 @@
+"""Various mathematical functions that are useful for performing stylometry.
+"""
+from numpy.linalg import norm
+
+def cosine_distance(m, f):
+    """Compute the cosine distance between a "model" M from an author's set of
+    documents and a featureset F extracted from D (the document to anonymize.)
+    Note that m and f must be of equal length for this computation to be
+    meaningful.
+
+    :param m: A set of averages of every author's feature.
+    :param f: The featureset extracted from D.
+    :returns: A distance such that, given cosine_distance(x, y) <
+    cosine_distance(x, z), we say that x is "closer to" y than z.
+    """
+    return (1 - ((m * f).sum()/(norm(m) * norm(f))))

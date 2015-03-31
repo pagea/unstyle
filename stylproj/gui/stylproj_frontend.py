0dc5192c7b8aa74e27c2c47cdc0c054e435b5b93
Alden S. Page <pagea@allegheny.edu>
2015-03-31 17:12:56 -0400

    Proper feature names and descriptions are displayed

--- stylproj/gui/stylproj_frontend.py
+++ stylproj/gui/stylproj_frontend.py
@@ -5,19 +5,55 @@ from PyQt5 import QtGui
 from stylproj.gui.stylproj_auto import Ui_MainWindow
 import stylproj.controller
 
+
 class StylProj(QMainWindow):
     def __init__(self, parent=None):
         # Initialized the generated interface code.
         super(StylProj, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
+        self.featureRows = {}
         # Signal connections
         self.ui.stackedNext.clicked.connect(self.stackNext_clicked)
         self.ui.browseYourDoc.clicked.connect(self.browseYourDoc_clicked)
         self.ui.browseYourDocs.clicked.connect(self.browseYourDocs_clicked)
         self.ui.deleteYourDocs.clicked.connect(self.deleteYourDocs_clicked)
-        self.ui.textEdit.textChanged.connect(self.changedText)
+        self.ui.textEdit.textChanged.connect(self.refreshAnonymity)
+        self.ui.rankTable.selectionModel().selectionChanged.connect(self.row_highlighted)
+    def getFeatureDesc(self, functionName):
+        """Translate feature extractor names into something that the end user
+        can understand.
+        :param functionName: A feature extracting function.
+        :returns: A typle containing ("Feature Name", "Description of feature").
+        """
+        names = {
+            "letterSpace" : ("Letter Space",
+                            ("The total number of letters appearing in your "
+                            "document.")),
+            "gunningFog" : ("Gunning-Fog readability",
+                           ("A function related to "
+                            "the ratio of words/sentences and complex word/total words.")),
+            "avgSyllablesPerWord" : ("Average syllables per word",
+                                    ("The total "
+                                     "number of syllables/the total number of words.")),
+            "unique_words" : ("Unique words",
+                             ("The number of words that appear "
+                              "only once in your document.")),
+            "sentenceCount" : ("Sentence count",
+                              ("The number of sentences in your document.")),
+            "characterSpace" : ("Character space", ("The total number of "
+            "characters (letters and numbers) appearing in your document.")),
+            "avgSentenceLength" : ("Average sentence length",
+                                  ("The average "
+                                   "length of sentences in your document.")),
+            "complexity" : ("Complexity",
+                           ("The ratio of unique words to total"
+                            "words in your document.")),
+            "fleschReadingEase" : ("Flesch readability",
+                                  ("A function related to"
+                                   " the ratio of words/sentences and syllables/words."))
+        }
+        return names[functionName]
 
     # stackedWidget buttons
     def stackNext_clicked(self):
@@ -56,7 +92,7 @@ class StylProj(QMainWindow):
     # TODO: Rather than check anonymity every time the user changes the text,
     # have a separate thread check every 5 or 10 seconds. Otherwise, we're going
     # to be constantly locking up the interface when we use large featuresets.
-    def changedText(self):
+    def refreshAnonymity(self):
         """Called whenever the user changes the text editor.
         """
         # Make sure we've trained the classifier before trying to do any
@@ -70,10 +106,6 @@ class StylProj(QMainWindow):
             self.ui.anonStatus.setText(("It is still possible to identify you as the "
                                  "author. Continue changing your document."))
         if anonymity is 1:
-            # FIXME: For reasons unknown, qt absolutely will not load the warning icon,
-            # or any other icon besides x and check. This is in spite of the
-            # fact that the icon shows just fine in designer. As a workaround,
-            # we just use x.
             self.ui.anonIcon.setPixmap(QtGui.QPixmap(":/icons/img/w.png"))
             self.ui.anonStatus.setText(("Although you are not the most likely author,"
                                  " there is a statistically significant chance"
@@ -85,8 +117,18 @@ class StylProj(QMainWindow):
                                  " document is no longer associated with your"
                                  " identity."))
 
+    def row_highlighted(self, _ , __):
+        """Every time someone selects a row from the table, we update our
+        description box with the description of the feature.
+        """
+        selected = self.ui.rankTable.selectionModel().selectedRows()[0].row()
+        featureHighlighted = self.featureRows[selected]
+        # Display the description of the highlighted feature
+        self.ui.featureDescription.setText(self.getFeatureDesc(featureHighlighted)[1])
+
     # Controller messages
     def update_stats(self):
+        self.refreshAnonymity()
         # Set up rank table dimensions
         self.ui.rankTable.setRowCount(len(stylproj.controller.feature_ranks))
         # Name the headers of the table
@@ -97,8 +139,8 @@ class StylProj(QMainWindow):
 
         tableHeight = (len(stylproj.controller.feature_ranks))
         # XXX: Sorting should be handled in the table, not in the
-        # rank_features methods. This will allow us to untangle this big ball of
-        # spaghetti code.
+        # rank_features methods. This will allow us to fix this embarrassingly
+        # overcomplicated code.
 
         # Fill in the feature column
         for idx, pair in enumerate(stylproj.controller.feature_ranks):
@@ -107,10 +149,10 @@ class StylProj(QMainWindow):
             # not exist.
             if currItem is None:
                 currItem = QTableWidgetItem(1)
-                currItem.setText(pair[0])
+                currItem.setText(self.getFeatureDesc(pair[0])[0])
                 self.ui.rankTable.setItem(idx, 0, currItem)
             else:
-                currItem.setText(feature_ranks[pair[0]])
+                currItem.setText(self.getFeatureDesc(feature_ranks[pair[0]])[0])
 
         # Initialize target and initial columns
         for idx, target in enumerate(stylproj.controller.targets): 
@@ -123,11 +165,13 @@ class StylProj(QMainWindow):
                 self.ui.rankTable.setItem(idx, 2, currItem2)
 
         # Populate target and current val columns
+        # Track feature table locations
         labelsBeforeSorting = stylproj.controller.featlabels
         for idx, label in enumerate(labelsBeforeSorting):
             for idx2, item in enumerate(range(tableHeight)):
                 currItem = self.ui.rankTable.item(item, 0)
-                if label == currItem.text():
+                if self.getFeatureDesc(label)[0] == currItem.text():
+                    self.featureRows[idx2] = label
                     print(label, " ", currItem.text(), " ", item)
                     currItem = self.ui.rankTable.item(item, 1)
                     currItem.setText(str(stylproj.controller.targets[idx]))

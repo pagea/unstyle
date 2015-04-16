8002cfa18ed2ca9dd352106ad938b5bd33f89ce9
Alden S. Page <pagea@allegheny.edu>
2015-04-16 18:27:59 -0400

    nltk should download packages correctly now

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 apt-get install python3 python3-pip
 pip3 install nltk scikit-learn numpy
-python3 -m nltk.download(punkt)
-python3 -m nltk.download(pickle)
+python3 -m nltk.downloader punkt cmudict
